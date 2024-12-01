18cc7ac1b12a28116769c2ed1270da411651f462
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-01 13:39:37 +0200

    day 1 improvements

--- day01.fsx
+++ day01.fsx
@@ -1,8 +1,8 @@
-let left, right =
+let left, right, lookup =
     System.IO.File.ReadAllLines "inputs/day01.txt"
     |> Array.map (fun line -> int line[..5], int line[8..])
     |> Array.unzip
-    |> fun (left, right) -> Array.sort left, Array.sort right
+    |> fun (left, right) -> Array.sort left, Array.sort right, right |> Array.countBy id |> Map
 
 (left, right) ||> Array.map2 (-) |> Array.sumBy abs |> printfn "Part 1: %A"
-left |> Array.sumBy (fun id -> id * (right |> Array.filter ((=) id) |> Array.length)) |> printfn "Part 2: %A"
+left |> Array.sumBy (fun id -> id * (lookup.TryFind id |> Option.defaultValue 0)) |> printfn "Part 2: %A"
