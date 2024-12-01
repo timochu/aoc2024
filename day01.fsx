7a22fe318e53963ddae2466c5ac9d7024ba3d689
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-01 14:11:41 +0200

    Day 01 formatting

--- day01.fsx
+++ day01.fsx
@@ -1,5 +1,13 @@
 let left, right = 
-    System.IO.File.ReadAllLines "day01.txt" |> Array.map (fun l -> int l[..5], int l[8..]) |> Array.unzip
+    System.IO.File.ReadAllLines "day01.txt" 
+    |> Array.map (fun l -> int l[..5], int l[8..]) 
+    |> Array.unzip
 
-(Array.sort left, Array.sort right) ||> Array.map2 (-) |> Array.sumBy abs |> printfn "Part 1: %i"
-left |> Array.sumBy (fun id -> id * (right |> Array.filter ((=) id)).Length) |> printfn "Part 2: %i"
+(Array.sort left, Array.sort right) 
+||> Array.map2 (-) 
+|> Array.sumBy abs 
+|> printfn "Part 1: %i"
+
+left 
+|> Array.sumBy (fun id -> id * (right |> Array.filter ((=) id)).Length) 
+|> printfn "Part 2: %i"
