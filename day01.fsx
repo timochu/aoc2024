0616f3ef0063263331b5709cc7e430fe1e574bb2
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-01 13:16:30 +0200

    Day 01 cleanup

--- day01.fsx
+++ day01.fsx
@@ -1,14 +1,8 @@
-let (l, r) =
+let l, r =
     System.IO.File.ReadAllLines "inputs/day01.txt"
-    |> Array.map (fun l -> int l[..5], int l[8..])
+    |> Array.map (fun line -> int line[..5], int line[8..])
     |> Array.unzip
-    |> (fun (left, right) -> Array.sort left, Array.sort right)
+    |> fun (left, right) -> Array.sort left, Array.sort right
 
-(l, r)
-||> Array.map2 (-)
-|> Array.sumBy abs
-|> printfn "Part 1: %A"
-
-l
-|> Array.sumBy (fun i -> i * (r |> Array.filter ((=) i) |> Array.length))
-|> printfn "Part 2: %A"
+(l, r) ||> Array.map2 (-) |> Array.sumBy abs |> printfn "Part 1: %A"
+l |> Array.sumBy (fun i -> i * (r |> Array.filter ((=) i) |> Array.length)) |> printfn "Part 2: %A"
