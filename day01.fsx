887ceee5d949be0fee0e938016b58422dc69b10e
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-01 13:11:08 +0200

    Improve day 01

--- day01.fsx
+++ day01.fsx
@@ -1,7 +1,14 @@
-let l, r = System.IO.File.ReadAllLines "inputs/day01.txt"
-           |> Array.map (fun l -> l.Split("   ") |> Array.map int)
-           |> (fun l -> l |> Array.map Array.head |> Array.sort, 
-                        l |> Array.map Array.last |> Array.sort)
-            
-(l, r) ||> Array.map2 (-) |> Array.sumBy abs |> printfn "Part 1: %A"
-l |> Array.sumBy (fun i -> i * (r |> Array.filter ((=) i) |> Array.length)) |> printfn "Part 2: %A"
+let (l, r) =
+    System.IO.File.ReadAllLines "inputs/day01.txt"
+    |> Array.map (fun l -> int l[..5], int l[8..])
+    |> Array.unzip
+    |> (fun (left, right) -> Array.sort left, Array.sort right)
+
+(l, r)
+||> Array.map2 (-)
+|> Array.sumBy abs
+|> printfn "Part 1: %A"
+
+l
+|> Array.sumBy (fun i -> i * (r |> Array.filter ((=) i) |> Array.length))
+|> printfn "Part 2: %A"
