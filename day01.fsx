7147724731243e8d48c539694a6e0b50465f2b34
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-01 14:12:01 +0200

    Day 01 whitespace cleanup

--- day01.fsx
+++ day01.fsx
@@ -1,13 +1,13 @@
 let left, right = 
-    System.IO.File.ReadAllLines "day01.txt" 
-    |> Array.map (fun l -> int l[..5], int l[8..]) 
+    System.IO.File.ReadAllLines "day01.txt"
+    |> Array.map (fun l -> int l[..5], int l[8..])
     |> Array.unzip
 
-(Array.sort left, Array.sort right) 
-||> Array.map2 (-) 
-|> Array.sumBy abs 
+(Array.sort left, Array.sort right)
+||> Array.map2 (-)
+|> Array.sumBy abs
 |> printfn "Part 1: %i"
 
 left 
-|> Array.sumBy (fun id -> id * (right |> Array.filter ((=) id)).Length) 
+|> Array.sumBy (fun id -> id * (right |> Array.filter ((=) id)).Length)
 |> printfn "Part 2: %i"
