2acf6189c43eb511fd79fba901391c8db0a16c28
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-02 20:42:00 +0200

    cleanup

--- day02.fsx
+++ day02.fsx
@@ -1,16 +1,16 @@
 open System; open type System.Convert
 
 let rec safe direction report =
-    match report, direction with
-    | _, d when d |> set |> Set.count > 1 -> false
-    | [], _ -> true
-    | (x, y) :: _, _ when abs (x - y) > 3 -> false
-    | (x, y) :: _, _ when abs (x - y) < 1 -> false
-    | (x, y) :: rest, d -> safe (Math.Sign(x - y) :: d) rest
+    match direction, report  with
+    | dir, _ when dir |> Set.count > 1  -> false
+    | _, []                             -> true
+    | _, (x, y) :: _ when abs (x-y) > 3 -> false
+    | _, (x, y) :: _ when abs (x-y) < 1 -> false
+    | dir, (x, y) :: rest               -> safe (dir.Add (Math.Sign(x - y))) rest
 
 let permute report = [ for i in 0 .. List.length report - 1 -> List.removeAt i report ]
 
 let reports = [for report in IO.File.ReadAllLines "day02.txt" -> [for level in report.Split() -> int level]]
 
-reports |> List.sumBy (List.pairwise >> safe [] >> ToInt32) |> printfn "Part 1: %i"
-reports |> List.sumBy (permute >> List.exists (List.pairwise >> safe []) >> ToInt32) |> printfn "Part 2: %i"
+reports |> List.sumBy (List.pairwise >> safe Set.empty >> ToInt32) |> printfn "Part 1: %i"
+reports |> List.sumBy (permute >> List.exists (List.pairwise >> safe Set.empty) >> ToInt32) |> printfn "Part 2: %i"
