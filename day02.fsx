198f2ce23363300b99f792b5755f4228e9e68057
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-02 19:05:50 +0200

    improve legibility day 02

--- day02.fsx
+++ day02.fsx
@@ -4,7 +4,8 @@ let rec safe direction report =
     | [] -> true
     | (x, y) :: _ when abs (x - y) > 3 || abs (x - y) < 1 -> false
     | (x, y) :: tail -> safe (direction |> Set.add (System.Math.Sign(x - y))) tail
+let permute (report : int list) = [ 0 .. report.Length - 1 ] |> List.map (fun i -> report |> List.removeAt i)
 
-let reports = System.IO.File.ReadAllLines "day02.txt" |> Array.map (fun l -> l.Split " " |> Array.map int |> Array.toList)
+let reports = System.IO.File.ReadAllLines "day02.txt" |> Array.map (fun l -> l.Split() |> Array.map int |> Array.toList)
 reports |> Array.filter (List.pairwise >> safe Set.empty) |> Array.length |> printfn "Part 1: %i"
-reports |> Array.filter (fun r -> [ 0 .. r.Length - 1 ] |> List.map (fun i -> r |> List.removeAt i) |> List.exists (List.pairwise >> safe Set.empty)) |> Array.length |> printfn "Part 2: %i"
+reports |> Array.filter (permute >> List.exists (List.pairwise >> safe Set.empty)) |> Array.length |> printfn "Part 2: %i"
