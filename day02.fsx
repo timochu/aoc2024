50155686df1c39197c6703eddade3d80a94ce12d
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-02 21:15:59 +0200

    cleanup

--- day02.fsx
+++ day02.fsx
@@ -1,12 +1,12 @@
 open System; open type System.Convert
 
-let rec safe direction report =
-    match direction, report  with
-    | dir, _ when dir |> Set.count > 1  -> false
-    | _, []                             -> true
-    | _, (x, y) :: _ when abs (x-y) > 3 -> false
-    | _, (x, y) :: _ when abs (x-y) < 1 -> false
-    | dir, (x, y) :: rest               -> safe (dir.Add (Math.Sign(x - y))) rest
+let rec safe (direction : int Set) report =
+    if direction.Count > 1 then false
+    else match report  with
+               | []                             -> true
+               | (x, y) :: _ when abs (x-y) > 3 -> false
+               | (x, y) :: _ when abs (x-y) < 1 -> false
+               | (x, y) :: rest                 -> safe (direction.Add (Math.Sign(x-y))) rest
 
 let permute report = [ for i in 0 .. List.length report - 1 -> List.removeAt i report ]
 
