538c591a0392210d783244d099569dfed65384ba
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-02 19:17:18 +0200

    Cleanup

--- day02.fsx
+++ day02.fsx
@@ -1,11 +1,15 @@
+open System
+
 let rec safe direction report =
     if Set.count direction > 1 then false else
     match report with
     | [] -> true
     | (x, y) :: _ when abs (x - y) > 3 || abs (x - y) < 1 -> false
-    | (x, y) :: tail -> safe (direction |> Set.add (System.Math.Sign(x - y))) tail
-let permute (report : int list) = [ 0 .. report.Length - 1 ] |> List.map (fun i -> report |> List.removeAt i)
+    | (x, y) :: tail -> safe (direction + set [Math.Sign(x - y)]) tail
+    
+let permute report = [ 0 .. Seq.length report - 1 ] |> List.map (fun i -> report |> List.removeAt i)
 
-let reports = System.IO.File.ReadAllLines "day02.txt" |> Array.map (fun l -> l.Split() |> Array.map int |> Array.toList)
-reports |> Array.filter (List.pairwise >> safe Set.empty) |> Array.length |> printfn "Part 1: %i"
-reports |> Array.filter (permute >> List.exists (List.pairwise >> safe Set.empty)) |> Array.length |> printfn "Part 2: %i"
+let reports = IO.File.ReadAllLines "day02.txt" |> Array.map (fun l -> l.Split() |> Array.map int |> Array.toList)
+
+reports |> Seq.sumBy (List.pairwise >> safe Set.empty >> Convert.ToInt32) |> printfn "Part 1: %i"
+reports |> Seq.sumBy (permute >> List.exists (List.pairwise >> safe Set.empty) >> Convert.ToInt32) |> printfn "Part 2: %i"
