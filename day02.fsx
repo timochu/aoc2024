beb131d1fa78752a82b215a14b00a6415a07ab3e
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-02 19:19:31 +0200

    golf

--- day02.fsx
+++ day02.fsx
@@ -1,4 +1,4 @@
-open System
+open System; open type System.Convert
 
 let rec safe direction report =
     if Set.count direction > 1 then false else
@@ -6,10 +6,10 @@ let rec safe direction report =
     | [] -> true
     | (x, y) :: _ when abs (x - y) > 3 || abs (x - y) < 1 -> false
     | (x, y) :: tail -> safe (direction + set [Math.Sign(x - y)]) tail
-    
+
 let permute report = [ 0 .. Seq.length report - 1 ] |> List.map (fun i -> report |> List.removeAt i)
 
 let reports = IO.File.ReadAllLines "day02.txt" |> Array.map (fun l -> l.Split() |> Array.map int |> Array.toList)
 
-reports |> Seq.sumBy (List.pairwise >> safe Set.empty >> Convert.ToInt32) |> printfn "Part 1: %i"
-reports |> Seq.sumBy (permute >> List.exists (List.pairwise >> safe Set.empty) >> Convert.ToInt32) |> printfn "Part 2: %i"
+reports |> Seq.sumBy (List.pairwise >> safe Set.empty >> ToInt32) |> printfn "Part 1: %i"
+reports |> Seq.sumBy (permute >> List.exists (List.pairwise >> safe Set.empty) >> ToInt32) |> printfn "Part 2: %i"
