9697bf54ae0714206f7c30506a789272dc4b7e51
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-03 16:29:02 +0200

    Day 3 formatting

--- day03.fsx
+++ day03.fsx
@@ -1,12 +1,7 @@
 open System.IO; open System.Text.RegularExpressions
 
-let mult = 
-    Regex(@"mul\((\d{1,3}),(\d{1,3})\)").Matches
-    >> Seq.sumBy (fun m -> (int m.Groups[1].Value) * (int m.Groups[2].Value))
-
-let clean s = 
-    Regex(@"don't\(\).*?(do\(\)|$)", RegexOptions.Singleline).Replace(s, "")
-    
+let mult = Regex(@"mul\((\d{1,3}),(\d{1,3})\)").Matches >> Seq.sumBy (fun m -> (int m.Groups[1].Value) * (int m.Groups[2].Value))
+let clean s = Regex(@"don't\(\).*?(do\(\)|$)", RegexOptions.Singleline).Replace(s, "")
 
 File.ReadAllText "day03.txt" |> mult |> printfn "Part 1: %i"
 File.ReadAllText "day03.txt" |> clean |> mult |> printfn "Part 2: %A"
