f68c57da23cbe65ef2f2dbce9e0cb6e4d091b6cf
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-03 16:20:58 +0200

    Day 03 part 2 regex solution

--- day03.fsx
+++ day03.fsx
@@ -1,4 +1,7 @@
 open System.Text.RegularExpressions
-let memory = System.IO.File.ReadAllText "day03.txt"
-let rx = Regex(@"mul\(([0-9]|[1-9][0-9]|[1-9][0-9][0-9]),([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\)", RegexOptions.Compiled)
-memory |> rx.Matches |> Seq.sumBy (fun m -> (int m.Groups[1].Value) * (int m.Groups[2].Value)) |> (printfn "Part 1: %i")
+let input = System.IO.File.ReadAllText "day03.txt"
+let mult = Regex(@"mul\(([0-9]|[1-9][0-9]|[1-9][0-9][0-9]),([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\)")
+let disabled = Regex(@"don't\(\).*?(do\(\)|$)", RegexOptions.Singleline)
+
+input |> mult.Matches |> Seq.sumBy (fun m -> (int m.Groups[1].Value) * (int m.Groups[2].Value)) |> (printfn "Part 1: %i")
+input |> (fun x -> disabled.Replace(x, "")) |> mult.Matches  |> Seq.sumBy (fun m -> (int m.Groups[1].Value) * (int m.Groups[2].Value)) |> printfn "Part 2: %A"
