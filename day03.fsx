ff9fb6fa621f7ae1d2e4310685e7b30694c43d37
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-03 16:27:39 +0200

    Improve day 3 regex

--- day03.fsx
+++ day03.fsx
@@ -1,7 +1,7 @@
 open System.IO; open System.Text.RegularExpressions
 
 let mult = 
-    Regex(@"mul\(([0-9]|[1-9][0-9]|[1-9][0-9][0-9]),([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\)").Matches
+    Regex(@"mul\((\d{1,3}),(\d{1,3})\)").Matches
     >> Seq.sumBy (fun m -> (int m.Groups[1].Value) * (int m.Groups[2].Value))
 
 let clean s = 
