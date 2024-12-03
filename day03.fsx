9f2edcb000ccb325296a97675d11cc8bc68cb8d2
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-03 16:26:52 +0200

    Day 03 clean

--- day03.fsx
+++ day03.fsx
@@ -1,7 +1,12 @@
-open System.Text.RegularExpressions
-let input = System.IO.File.ReadAllText "day03.txt"
-let mult = Regex(@"mul\(([0-9]|[1-9][0-9]|[1-9][0-9][0-9]),([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\)")
-let disabled = Regex(@"don't\(\).*?(do\(\)|$)", RegexOptions.Singleline)
+open System.IO; open System.Text.RegularExpressions
 
-input |> mult.Matches |> Seq.sumBy (fun m -> (int m.Groups[1].Value) * (int m.Groups[2].Value)) |> (printfn "Part 1: %i")
-input |> (fun x -> disabled.Replace(x, "")) |> mult.Matches  |> Seq.sumBy (fun m -> (int m.Groups[1].Value) * (int m.Groups[2].Value)) |> printfn "Part 2: %A"
+let mult = 
+    Regex(@"mul\(([0-9]|[1-9][0-9]|[1-9][0-9][0-9]),([0-9]|[1-9][0-9]|[1-9][0-9][0-9])\)").Matches
+    >> Seq.sumBy (fun m -> (int m.Groups[1].Value) * (int m.Groups[2].Value))
+
+let clean s = 
+    Regex(@"don't\(\).*?(do\(\)|$)", RegexOptions.Singleline).Replace(s, "")
+    
+
+File.ReadAllText "day03.txt" |> mult |> printfn "Part 1: %i"
+File.ReadAllText "day03.txt" |> clean |> mult |> printfn "Part 2: %A"
