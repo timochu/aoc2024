fa13d3b00b06c90b9431431a6d1778c0b9f26c74
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-06 00:42:22 +0200

    Day 5 cleanup

--- day05.fsx
+++ day05.fsx
@@ -1,11 +1,7 @@
 let input = System.IO.File.ReadAllLines "day05.txt"
 let rules = [for l in input[..1175] -> int l[..1], int l[3..]]
 
-let hits pages =
-    [ for (r1, r2) in rules do
-        match List.tryFindIndex ((=) r1) pages, List.tryFindIndex ((=) r2) pages with
-        | Some i1, Some i2 when i1 > i2 -> yield (r1, r2)
-        | _ -> () ]
+let middle (arr : 'a list) = arr.[arr.Length / 2]
 
 let rec swap pages =
     (pages, rules) 
@@ -15,7 +11,8 @@ let rec swap pages =
         | _ -> acc)
     |> fun reordered -> if List.forall2 (=) pages reordered then reordered else swap reordered
 
-let middle (arr : 'a list) = arr.[arr.Length / 2]
-let printable, unprintable = [for l in input[1177..] -> [for i in l.Split(',') -> int i]] |> List.partition (hits >> List.isEmpty)
-printable |> List.sumBy middle |> printfn "Part 1: %i"
-unprintable |> List.sumBy (swap >> middle) |> printfn "Part 2: %i"
+[for l in input[1177..] -> [for i in l.Split(',') -> int i]] 
+|> List.partition (fun orig -> List.forall2 (=) (swap orig) orig)
+|> fun (printable, unprintable) ->
+    printable |> List.sumBy middle |> printfn "Part 1: %i"
+    unprintable |> List.sumBy (swap >> middle) |> printfn "Part 2: %i"
