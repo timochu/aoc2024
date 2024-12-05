a808fdfe52fde6d70594e00f3a29f21c3276f359
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-06 00:33:33 +0200

    Uncurse

--- day05.fsx
+++ day05.fsx
@@ -7,13 +7,14 @@ let hits pages =
         | Some i1, Some i2 when i1 > i2 -> yield (r1, r2)
         | _ -> () ]
 
-let rec swapper (rules : ((int*int) list)) (pages : int list) =
-    match rules |> List.randomShuffle with // secret sauce 😙🤌
-    | [] -> pages
-    | rules -> (pages, rules) 
-               ||> List.fold (fun acc (r1, r2) -> (acc |> List.findIndex ((=) r1), acc |> List.findIndex ((=) r2)) |> fun (i1, i2) -> acc |> List.updateAt i1 r2 |> List.updateAt i2 r1)
-               |> (fun p -> hits p, p) ||> swapper
+let rec swapper pages =
+    (pages, rules) 
+        ||> List.fold (fun acc (r1, r2) -> 
+            match (acc |> List.tryFindIndex ((=) r1), acc |> List.tryFindIndex ((=) r2)) with 
+            |Some i1, Some i2 when i1 > i2 -> acc |> List.updateAt i1 r2 |> List.updateAt i2 r1
+            | _ -> acc)
+        |> fun reordered -> if List.forall2 (=) pages reordered then reordered else swapper reordered
 
 let printable, unprintable = [for l in input[1177..] -> [for i in l.Split(',') -> int i]] |> List.partition (hits >> List.isEmpty)
 printable |> List.sumBy (fun u -> u.[u.Length / 2]) |> printfn "Part 1: %i"
-unprintable |> List.map (fun u -> swapper (hits u) u) |> List.sumBy (fun u -> u.[u.Length / 2]) |> printfn "Part 2: %i"
+unprintable |> List.map (fun u -> swapper u) |> List.sumBy (fun u -> u.[u.Length / 2]) |> printfn "Part 2: %i"
