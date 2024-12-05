1c7273616a33825e8f1a4a772f3a5d84d894a324
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-05 22:31:20 +0200

    golfing

--- day05.fsx
+++ day05.fsx
@@ -2,22 +2,22 @@ let input = System.IO.File.ReadAllLines "day05.txt"
 let rules = input |> Array.takeWhile (fun i -> i.Contains "|") |> Array.map (fun i -> int i[..1], int i[3..])
 let updates = input |> Array.skipWhile (fun i -> i.Contains "," |> not) |> Array.map (fun i -> i.Split(',') |> Array.map int)
 
-let hits (pages: int array) =
+let hits pages =
     [ for (r1, r2) in rules do
         match Array.tryFindIndex ((=) r1) pages, Array.tryFindIndex ((=) r2) pages with
         | Some i1, Some i2 when i1 > i2 -> yield (r1, r2)
         | _ -> () ]
 
-let sorter (pages : int array)  =
-    let rec swapper (rules : ((int*int) List)) (pages : int array) =
+let sorter pages  =
+    let rec swapper rules pages =
         match rules |> List.randomShuffle with // secret sauce 😙🤌
         | [] -> pages
         | rules ->
             for (r1,r2) in rules do
                 let i1 = pages |> Array.findIndex ((=) r1)
                 let i2 = pages |> Array.findIndex ((=) r2)
-                Array.set pages i1 r2 
                 Array.set pages i2 r1
+                Array.set pages i1 r2
             swapper (hits pages) pages
     swapper (hits pages) pages
 
