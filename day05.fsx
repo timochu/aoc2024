3dec20d715f60db727ecf164b972e2a58e721870
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-05 23:25:30 +0200

    golf day 5

--- day05.fsx
+++ day05.fsx
@@ -1,20 +1,20 @@
 let input = System.IO.File.ReadAllLines "day05.txt"
-let rules = input |> Array.takeWhile (fun i -> i.Contains "|") |> Array.map (fun i -> int i[..1], int i[3..])
+let rules = [for l in input[..1175] -> int l[..1], int l[3..]]
 
 let hits pages =
     [ for (r1, r2) in rules do
-        match Array.tryFindIndex ((=) r1) pages, Array.tryFindIndex ((=) r2) pages with
+        match List.tryFindIndex ((=) r1) pages, List.tryFindIndex ((=) r2) pages with
         | Some i1, Some i2 when i1 > i2 -> yield (r1, r2)
         | _ -> () ]
 
-let rec swapper (rules : ((int*int) List)) (pages : int array) =
+let rec swapper (rules : ((int*int) list)) (pages : int list) =
     match rules |> List.randomShuffle with // secret sauce 😙🤌
     | [] -> pages
     | rules -> (pages, rules) ||> List.fold (fun acc (r1, r2) -> 
-                let i1, i2 = acc |> Array.findIndex ((=) r1), acc |> Array.findIndex ((=) r2)
-                acc |> Array.updateAt i1 r2 |> Array.updateAt i2 r1)
-            |> (fun p -> hits p, p) ||> swapper
+                                                let i1, i2 = acc |> List.findIndex ((=) r1), acc |> List.findIndex ((=) r2)
+                                                acc |> List.updateAt i1 r2 |> List.updateAt i2 r1)
+               |> (fun p -> hits p, p) ||> swapper
 
-let printable, unprintable = input |> Array.skipWhile (fun i -> i.Contains "," |> not) |> Array.map (fun i -> i.Split(',') |> Array.map int) |> Array.partition (hits >> List.isEmpty)
-printable |> Array.sumBy (fun u -> u.[u.Length / 2]) |> printfn "Part 1: %i"
-unprintable |> Array.map (fun u -> swapper (hits u) u) |> Array.sumBy (fun u -> u.[u.Length / 2]) |> printfn "Part 2: %i"
+let printable, unprintable = [for l in input[1177..] -> [for i in l.Split(',') -> int i]] |> List.partition (hits >> List.isEmpty)
+printable |> List.sumBy (fun u -> u.[u.Length / 2]) |> printfn "Part 1: %i"
+unprintable |> List.map (fun u -> swapper (hits u) u) |> List.sumBy (fun u -> u.[u.Length / 2]) |> printfn "Part 2: %i"
