e2873a1cf43c3f1d47a8cc611f2ba8e22a82774d
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-05 17:50:27 +0200

    Day 05 part 1 cleanup

--- day05.fsx
+++ day05.fsx
@@ -1,12 +1,13 @@
-let input = System.IO.File.ReadAllLines "day05.txt"
+let input = System.IO.File.ReadAllLines "day05e.txt"
 let rules = input |> Array.takeWhile (fun i -> i.Contains "|") |> Array.map (fun i -> int i[..1], int i[3..])
 let updates = input |> Array.skipWhile (fun i -> i.Contains "," |> not) |> Array.map (fun i -> i.Split(',') |> Array.map int)
 
-let isCorrect (pages : int array) = 
-    rules |> Array.forall (fun (r1, r2) -> 
-        if pages |> Array.contains r1 && pages |> Array.contains r2 then
-            (pages |> Array.findIndex ((=) r1)) < (pages |> Array.findIndex ((=) r2))
-        else true
-    )
+let hits (pages : int array) : (int * int) seq = 
+    seq { 
+        for (r1, r2) in rules do
+        if pages |> Array.contains r1 && pages |> Array.contains r2 &&
+            (pages |> Array.findIndex ((=) r1)) > (pages |> Array.findIndex ((=) r2)) then
+                yield r1, r2
+    }
 
-updates |> Array.where isCorrect |> Array.sumBy (fun u -> u.[u.Length/2]) |> printfn "Part 1: %i"
+updates |> Array.where (hits >> Seq.isEmpty) |> Array.sumBy (fun u -> u.[u.Length/2]) |> printfn "Part 1: %i"
