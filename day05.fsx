4e8f5328d661ed6d529a0f8602e7cf34ee597a66
Timo Vahtera <timo.vahtera@gmail.com>
2024-12-05 22:27:15 +0200

    Some cleanup

--- day05.fsx
+++ day05.fsx
@@ -3,25 +3,23 @@ let rules = input |> Array.takeWhile (fun i -> i.Contains "|") |> Array.map (fun
 let updates = input |> Array.skipWhile (fun i -> i.Contains "," |> not) |> Array.map (fun i -> i.Split(',') |> Array.map int)
 
 let hits (pages: int array) =
-    pages, [ for (r1, r2) in rules do
-                match Array.tryFindIndex ((=) r1) pages, Array.tryFindIndex ((=) r2) pages with
-                | Some i1, Some i2 when i1 > i2 -> yield (r1, r2)
-                | _ -> () ]
+    [ for (r1, r2) in rules do
+        match Array.tryFindIndex ((=) r1) pages, Array.tryFindIndex ((=) r2) pages with
+        | Some i1, Some i2 when i1 > i2 -> yield (r1, r2)
+        | _ -> () ]
 
-let rec sorter (pages : int array) (rules : ((int*int) List)) =
-    match rules |> List.randomShuffle with // secret sauce 😙🤌
-    | [] -> pages
-    | rules ->
-        for (r1,r2) in rules do
-            let i1 = pages |> Array.findIndex ((=) r1)
-            let i2 = pages |> Array.findIndex ((=) r2)
-            Array.set pages i1 r2 
-            Array.set pages i2 r1
-        sorter pages (hits pages |> snd)
-        
-updates |> Array.where (hits >> snd >> List.isEmpty) |> Array.sumBy (fun u -> u.[u.Length / 2]) |> printfn "Part 1: %i"
-updates 
-|> Array.map hits 
-|> Array.where (snd >> Seq.isEmpty >> not)
-|> Array.map (fun (p, r) -> sorter p r)
-|> Array.sumBy (fun u -> u.[u.Length / 2]) |> printfn "Part 2: %i"
+let sorter (pages : int array)  =
+    let rec swapper (rules : ((int*int) List)) (pages : int array) =
+        match rules |> List.randomShuffle with // secret sauce 😙🤌
+        | [] -> pages
+        | rules ->
+            for (r1,r2) in rules do
+                let i1 = pages |> Array.findIndex ((=) r1)
+                let i2 = pages |> Array.findIndex ((=) r2)
+                Array.set pages i1 r2 
+                Array.set pages i2 r1
+            swapper (hits pages) pages
+    swapper (hits pages) pages
+
+updates |> Array.where (hits >> List.isEmpty) |> Array.sumBy (fun u -> u.[u.Length / 2]) |> printfn "Part 1: %i"
+updates |> Array.where (hits >> Seq.isEmpty >> not) |> Array.map sorter |> Array.sumBy (fun u -> u.[u.Length / 2]) |> printfn "Part 2: %i"
